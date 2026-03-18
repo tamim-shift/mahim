0801e040ab0647feaf4610ab37a1ae5f7bff0297
— 𝐌𝐚𝐡𝐢𝐦 𝐈𝐬𝐥𝐚𝐦 🥳🍒 <209479334+mahim4u@users.noreply.github.com>
2026-03-18 07:03:00 +0600

    Update credits and message format in antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
   version: "0.0.1",
-  credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
+  credits: "MAHIM ISLAM",
   description: "Against changing Bot's nickname"
 };
 
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - 𝐏𝐚𝐠𝐨𝐥 𝐜𝐡𝐚𝐠𝐨𝐥 𝐭𝐮𝐢 𝐧𝐢𝐜𝐤𝐧𝐚𝐦𝐞 𝐜𝐡𝐚𝐧𝐠𝐞 𝐤𝐨𝐫𝐭𝐞 𝐩𝐚𝐫𝐛𝐢 𝐧𝐚! 😹`}, threadID);
     }  
         }
