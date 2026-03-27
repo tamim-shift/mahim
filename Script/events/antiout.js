a5ba0789b770f6d99765ed2c5380f8a1f106875b
— 𝐌𝐚𝐡𝐢𝐦 𝐈𝐬𝐥𝐚𝐦 🥳🍒 <209479334+mahim4u@users.noreply.github.com>
2026-03-27 15:10:17 +0600

    Update antiout.js

--- Script/events/antiout.js
+++ Script/events/antiout.js
@@ -15,7 +15,7 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
-    api.sendMessage(`𝐎𝐨𝐩𝐬! 𝐬𝐨𝐫𝐫𝐲 𝐛𝐨𝐬𝐬 𝐈 𝐜𝐚𝐧'𝐭 𝐫𝐞-𝐚𝐝𝐝 ${name} to the group...! 🥲`, event.threadID)
+    api.sendMessage(`𝐎𝐨𝐩𝐬! 𝐬𝐨𝐫𝐫𝐲 𝐛𝐨𝐬𝐬 𝐈 𝐜𝐚𝐧'𝐭 𝐫𝐞-𝐚𝐝𝐝 ${name} 𝐭𝐨 𝐭𝐡𝐞 𝐠𝐫𝐨𝐮𝐩...! 🥲`, event.threadID)
    } else api.sendMessage(`𝐑𝐞-𝐚𝐝𝐝𝐞𝐝 ${name} 𝐭𝐨 𝐭𝐡𝐞 𝐠𝐫𝐨𝐮𝐩...! 🙂`, event.threadID);
   })
  }
